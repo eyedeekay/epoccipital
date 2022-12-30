624fa1bdc952ee61cceb210b7039ccfb8b4d3f1a
idk <hankhill19580@gmail.com>
2022-12-30 04:06:37 +0000

    add README generator

---
+++ readme.sh
@@ -0,0 +1,27 @@
+
+epoccipital --help | tee README.md
+echo "" | tee -a README.md
+epoccipital  apikeys --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  completion --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  debug --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  generate --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  help --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  hiddenserve --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  mockoidc --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  namespaces --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  nodes --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  preauthkeys --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  routes --help | tee -a README.md
+echo "" | tee -a README.md
+epoccipital  version --help | tee -a README.md
+echo "" | tee -a README.md
